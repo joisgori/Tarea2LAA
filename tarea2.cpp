5230f5379e84f4bdafa0b9b0eac12c3a3adb00a5
joisgori <37356528+joisgori@users.noreply.github.com>
2019-08-30 21:19:58 -0600

    Primer commit

---
+++ tarea2.cpp
@@ -0,0 +1,24 @@
+/*
+  Escribir un programa que le pida enteros al usuario, guarde los números pares en una pila u los impares en otra pila.
+  Determinar si la cantidad de ambas pilas es igual
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Nodo{
+    int dato;
+    Nodo *sig;
+}*inicio;
+
+class Pila {
+public:
+
+};
+
+int main(int argc, const char * argv[]) {
+    //Crear una entrada de datos del usuario
+    return 0;
+}
